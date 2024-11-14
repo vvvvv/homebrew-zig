437120d66b8c2787591997149904d1d656f01903
v <vvvvv@vvvvv.io>
2024-11-14 19:38:42 +0100

    testing GitHub action

--- Casks/zig@nightly.rb
+++ Casks/zig@nightly.rb
@@ -4,10 +4,10 @@ cask "zig@nightly" do
   homepage "https://ziglang.org/"
 
   arch arm: "aarch64", intel: "x86_64"
-  version "0.14.0-dev.2245+4fc295dc0"
+  version ""
 
-  sha256 arm: "7ec392c82fd9ff9f9f5220c8632ee72e77d513743b39cc650440aa35ec6970cc",
-        intel: "673439791e0473c34a7cf405ce2b03ea8a21748ab4bf464ddd765eafd45802dc"
+  sha256 arm: "",
+        intel: ""
 
   url "https://ziglang.org/builds/zig-macos-#{arch}-#{version}.tar.xz"
 

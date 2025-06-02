44e0fb046d6d1ab4ea60d6f3f5b4d38a70c812db
v <v@vvvvv.io>
2025-06-03 01:19:40 +0200

    FIX: upstream url format change
    Upstream changed the format of the url serving the tarball. (See issue #2)
    This went unnoticed because the workflow script lacked proper error handling.
    This commit not only fixes this but also significantly improves the workflow script by:
    - Verifying that the url can actually be resolved
    - Handling more edge cases
    - Adding proper error handling

--- .github/workflows/scripts/update_zig_nightly.sh
+++ .github/workflows/scripts/update_zig_nightly.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+# set -x
 
 cask_file="Casks/zig@nightly.rb"
 
 # fetch the latest build info
 json_url="https://ziglang.org/download/index.json"
-json_data=$(curl -s "${json_url}")
-
-if [ -z "${json_data}" ]; then
+json_data=$(curl --silent --fail "${json_url}")
+if [[ "$?" -ne 0 || -z "${json_data}" ]]; then
   echo "Failed to fetch JSON data from ${json_url}"
   exit 1
 fi
 
-if [ ! -f "${cask_file}" ]; then
+if [[ ! -f "${cask_file}" ]]; then
   echo "Cask file not found at ${cask_file}"
   exit 1
 fi
@@ -21,31 +20,59 @@ fi
 content=$(cat "${cask_file}")
 
 # get latest version
-latest_version=$(jq -r ".master.version" <<<"${json_data}")
+latest_version=$(jq --exit-status -r ".master.version" <<<"${json_data}")
+if [[ "$?" -ne 0 ]]; then
+  printf 'No master.version found in json data\n%s' "${json_data}"
+  exit 1
+fi
 
 # update version line in cask file
-content=$(sed -E "s/version \"[^\"]*\"/version \"${latest_version}\"/" <<<"${content}")
+content=$(sed 's/version."[^"]*"/version "'"${latest_version}"'"/' <<<"${content}")
 
 # it's important that both arrays are in the same order
-declare -a arch_labels=( "arm" "intel" )
-declare -a archs=( "aarch64-macos" "x86_64-macos" )
+declare -a arch_labels=("arm" "intel")
+declare -a archs=("aarch64-macos" "x86_64-macos")
 
-if [[ "${#arch_labels[@]}" -gt "${#archs[@]}" ]]; then 
+if [[ "${#arch_labels[@]}" -gt "${#archs[@]}" ]]; then
   echo "Cask stanza expects archs '${arch_labels[@]}' but $0 is configured to only fetch '${archs[@]}'"
   exit 1
 fi
 
 declare -a shasums
 
-# extract shasums from fetched data
+# for every arch extract the shasum and check if the file is being served at the expected url
 for i in "${!archs[@]}"; do
+  # extract shasums from fetched data
   arch="${archs[$i]}"
-  build_info=$(jq -r ".master.\"${arch}\"" <<<"${json_data}")
-  if [[ "${build_info}" =~ ^null$ ]]; then
+  build_info=$(jq --exit-status -r ".master.\"${arch}\"" <<<"${json_data}")
+  if [[ "$?" -ne 0 ]]; then
+    # if [[ "${build_info}" =~ ^null$ ]]; then
     echo "No data found for arch ${arch_labels[$i]}/${arch}"
-    exit 1 
+    exit 1
   fi
-  shasums+=( "$(jq -r '.shasum' <<<"${build_info}")" )
+
+  build_shasum="$(jq --exit-status -r '.shasum' <<<"${build_info}")"
+  if [[ "$?" -ne 0 ]]; then
+    echo "No shasum found for arch ${arch_labels[$i]}/${arch}"
+  fi
+
+  shasums+=( "${build_shasum}" )
+
+
+  # check if file is being served from the expected url (see issue #2)
+  #
+  # the cask file contains a line which looks like this:
+  #   url "https://ziglang.org/builds/zig-#{arch}-#{version}.tar.xz"
+  # the sed expression substitutes #{arch} and #{version} with the actual arch and version respectively
+  url="$(sed -n \
+    '/url.*https:\/\/ziglang.org/{ s%.*"\(https://.*\)"%\1%;s%#{arch}%'"${arch}"'%;s%#{version}%'"${latest_version}"'%p; }' \
+    <<<"${content}")"
+
+  http_status="$(curl --head --write-out "%{http_code}" --silent --fail --output /dev/null "${url}")"
+  if [[ "$?" -ne 0 ]]; then
+    echo "Failed to resolve ${url}: HTTP STATUS ${http_status}"
+    exit 1
+  fi
 done
 
 if [[ "${#arch_labels[@]}" -ne "${#shasums[@]}" ]]; then
@@ -53,10 +80,9 @@ if [[ "${#arch_labels[@]}" -ne "${#shasums[@]}" ]]; then
   exit 1
 fi
 
-
 # replace sha256 sums with fetched ones
-content="$(awk -v arch_labels="$(tr ' ' ';' <<<"${arch_labels[@]}" )" -v shasums="$(tr ' ' ';' <<<"${shasums[@]}" )" \
-        'BEGIN{
+content="$(awk -v arch_labels="$(tr ' ' ';' <<<"${arch_labels[@]}")" -v shasums="$(tr ' ' ';' <<<"${shasums[@]}")" \
+  'BEGIN{
           split(arch_labels,archs,";")
           split(shasums,sums,";")
           i=1
@@ -83,9 +109,16 @@ content="$(awk -v arch_labels="$(tr ' ' ';' <<<"${arch_labels[@]}" )" -v shasums
           }
         } 
     { 
-      # print all lines which arent matching the expression above aswell
+      # print all lines which arent matching the expression above 
       print $0 
-    } ' <<<"${content}" )"
+    } ' <<<"${content}")"
 
-echo "${content}" > "${cask_file}"
-echo "Updated zig@nightly cask to version ${latest_version}"
+diff -q <(printf '%s\n' "${content}") "${cask_file}" &>/dev/null
+if [[ "$?" -eq 0 ]]; then
+  echo "Already at the latest version."
+  exit 0
+else
+  printf '%s\n' "${content}" >"${cask_file}"
+  echo "Updated zig@nightly cask to version ${latest_version}"
+  exit 0
+fi
--- Casks/zig@nightly.rb
+++ Casks/zig@nightly.rb
@@ -3,15 +3,14 @@ cask "zig@nightly" do
   desc "Programming language for robustness, optimality, and maintainability (Nightly Build)"
   homepage "https://ziglang.org/"
 
-  arch arm: "aarch64", intel: "x86_64"
-  version "0.15.0-dev.671+c907866d5"
+  arch arm: "aarch64-macos", intel: "x86_64-macos"
+  version ""
 
-  sha256 arm: "1dfb6017befa543fffbf3ba4d28b9c7c16a8b44dccde9560b57fa4c83f5bb4ea",
-        intel: "0766d487b033f727cbd614fb189212dff491bdd12ae1d65189884aa790dc255c"
+  sha256 arm: "",
+        intel: ""
 
-  url "https://ziglang.org/builds/zig-macos-#{arch}-#{version}.tar.xz"
+  url "https://ziglang.org/builds/zig-#{arch}-#{version}.tar.xz"
 
-  binary "zig-macos-#{arch}-#{version}/zig"
+  binary "zig-#{arch}-#{version}/zig"
 
-  # zap trash: "~/.zig"
 end
